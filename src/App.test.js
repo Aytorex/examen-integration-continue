9296ce7e24150434a6817d9985b1fdf8367adf42
Aytorex <joshua.kaczmarek@gmail.com>
2022-05-16 16:13:22 +0200

    test coverage 75%

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
 });
+
+// test("click on enter", () => {
+//     const inputElement = document.getElementById("input");
+//     //act(() => {
+//     fireEvent.change(inputElement, {target: {value:'a'}})
+//     fireEvent.keyDown(domNode, {key: 'Enter', code: 'Enter', charCode: 13})
+//     //})
+//     const outputElement = document.querySelector("ul>li:nth-child(1)");
+//     console.log(outputElement)
+//     //expect()
+//
+// })
